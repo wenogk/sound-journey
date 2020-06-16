3464d191fc3895fd2ca10805291297416a7117e5
wenogk <drf325@nyu.edu>
2020-06-16 21:32:21 +0400

    animations kinda working

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
 let paused = true;
-//214 seconds total
-//1:44 thunder - 104
-let totalSeconds = 214;
+//219 seconds total
+//32 second announcment
+//2:28 woman scream - 148
+let totalSeconds = 219;
 tl = gsap.timeline({
    paused: true
  })
  .to("#overlay", { //#727272
   duration:1,
-  background: "rgb(144,238,144,0.7)",
+  background: "rgb(144,238,144,0.4)",
   ease: "sine.out",
 },4)
 .to("#overlay", { //#727272
@@ -15,6 +16,26 @@ tl = gsap.timeline({
  background: "rgba(0,0,0,0)",
  ease: "sine.out"
 },5)
+.to("#overlay", { //#727272
+ duration:0.5,
+ background: "rgb(144,238,144,0.4)",
+ ease: "sine.out",
+},31.5)
+.to("#overlay", { //#727272
+duration:0.5,
+background: "rgba(0,0,0,0)",
+ease: "sine.out"
+},32)
+.to("#overlay", { //#727272
+ duration:0.5,
+ background: "rgb(144,238,144,0.4)",
+ ease: "sine.out",
+},55.5)
+.to("#overlay", { //#727272
+duration:0.5,
+background: "rgba(0,0,0,0)",
+ease: "sine.out"
+},56)
 .to("#overlay", {
   duration:0.4,
   background: "rgba(0,0,0,0.7)",
@@ -45,73 +66,80 @@ tl = gsap.timeline({
   top:"0px",
   ease: Bounce.easeOut
 },108)
-.to("#overlay", {
+.to("#overlay", { //fade to black
   duration:(totalSeconds-106),
   background: "black",
   ease: "sine.out"
 },106)
-.to("body", {
-  duration:0.5,
-  yoyo: true,
-  repeat: 76,
-  rotate: "+=3",
-  ease: Quad.easeInOut
-},112,"shake")
-.to("body", {
-  duration:0.5,
-  yoyo: true,
-  repeat: 76,
-  rotate: "-=3",
-  ease: Quad.easeInOut
-},112,"shake")
-.to(".mask1", {
-  duration:0.5,
-  yoyo: true,
-  repeat: 76,
-  rotate: "-=5",
-  ease: Quad.easeInOut
-},112,"shakee")
-.to(".mask1", {
-  duration:0.5,
-  yoyo: true,
-  repeat: 76,
-  rotate: "+=5",
-  ease: Quad.easeInOut
-},112,"shakee")
-.to("body", {
-  duration:0.3,
-  yoyo: true,
-  repeat: 70,
-  rotate: "+=5",
-  ease: Quad.easeInOut
-},150,"shake2")
-.to("body", { //2:30 150
-  duration:0.3,
-  yoyo: true,
-  repeat: 70,
-  rotate: "-=5",
-  ease: Quad.easeInOut
-},150,"shake2")
-.to("body", {
-  duration:0.1,
-  yoyo: true,
-  repeat: 58,
-  rotate: "+=5",
-  ease: Quad.easeInOut
-},185,"shake3")
-.to("body", { //2:30 150
-  duration:0.1,
-  yoyo: true,
-  repeat: 58,
-  rotate: "-=5",
-  ease: Quad.easeInOut
-},185,"shake3")
+
+
 var wavesurfer = WaveSurfer.create({
     container: '#waveform',
     waveColor: '#ccc',
     progressColor: '#343B3F',
     responsive: true
 });
+
+function turbulence(startTime,endTime,speed,angle) {
+  let repeat = Math.ceil((endTime - startTime) / speed);
+  let amountAdd = "+=" + angle;
+  let amountSubtract = "-=" + angle;
+  let startTime2 = startTime + speed;
+  let srotate = "rotate(-" + angle + "deg);"
+  console.log(startTime + " to " + endTime + " repeat " + repeat + " times for duration " + speed )
+  tl.to("body", {
+    duration: speed,
+    yoyo: true,
+    repeat: repeat,
+    rotate: amountAdd,
+    transformOrigin: '100% 50%',
+    ease: Quad.easeInOut
+  },startTime)
+  .to("body", {
+    duration: speed,
+    yoyo: true,
+    repeat: repeat,
+    rotate: amountSubtract,
+    transformOrigin: '50% 0%',
+    ease: Quad.easeInOut
+  },startTime2)
+  .to(".mask1", {
+    duration:speed,
+    yoyo: true,
+    repeat: repeat,
+    rotate: amountSubtract,
+    transformOrigin: '50% 0%',
+    ease: Quad.easeInOut
+  },startTime)
+  .to(".mask1", {
+    duration:speed,
+    yoyo: true,
+    repeat: repeat,
+    rotate: amountAdd,
+    transformOrigin: '50% 0%',
+    ease: Quad.easeInOut
+  },startTime2)
+  .to(".mask2", {
+    duration:speed,
+    yoyo: true,
+    repeat: repeat,
+    rotate: amountSubtract,
+    transformOrigin: '50% 0%',
+    ease: Quad.easeInOut
+  },startTime)
+  .to(".mask2", {
+    duration:speed,
+    yoyo: true,
+    repeat: repeat,
+    rotate: amountAdd,
+    transformOrigin: '50% 0%',
+    ease: Quad.easeInOut
+  },startTime2)
+}
+
+turbulence(112,148,0.5,3);
+turbulence(148,180,0.4,5);
+turbulence(180,197,0.2,5);
 wavesurfer.load('assets/final.mp3');
 wavesurfer.on('pause', function () {
   tl.pause();
@@ -148,21 +176,3 @@ wavesurfer.on('ready', function () {
      }
   });
 });
-
-  function shake(element="body"){
-  gsap.to(element, .1, {
-    x: -7,
-    ease: Quad.easeInOut
-  });
-  gsap.to(element, .1, {
-    repeat: 10,
-    x: 7,
-    yoyo: true,
-    delay: .1,
-    ease: Quad.easeInOut
-  });
-  gsap.to(element, .1, {
-    x: 0,
-    delay: .1 * 11
-  });
-}
